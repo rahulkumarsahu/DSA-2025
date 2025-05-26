a6e81171ca0e4be1f47178975b88785f28bebef4
Rahul Kumar <rahulkumar060995@gmail.com>
2025-05-26 22:37:40 +0530

    First

--- src/main/java/com/dsa2025/learn/array/two_pointer/DutchNationalFlagAlgo.java
+++ src/main/java/com/dsa2025/learn/array/two_pointer/DutchNationalFlagAlgo.java
@@ -3,7 +3,7 @@ package com.dsa2025.learn.array.two_pointer;
 // https://workat.tech/problem-solving/practice/dutch-national-flag
 
 import java.util.Arrays;
-
+//https://excalidraw.com/#json=8VBsZM5FUOyaGELqvsj1k,4v62HkSj_oqye_w3wPl6LA
 public class DutchNationalFlagAlgo {
 
     public static void main(String[] args) {
@@ -19,14 +19,25 @@ public class DutchNationalFlagAlgo {
 
         // here we are considering like in between low and mid is sorted 0,1
         // and mid to high is unsorted, so when mid-cross high we will break.
+        // dutch algo says like mid points encounter 0,1,2 and
+        // 0 ------> low - 1 -> 0
+        // low ------> mid - 1 -> 1
+        // mid ------> high -> unsorted 0,1,2
+        // high + 1 -----> n - 1 -> 2
+        // before low everything should be 0 and from low to mid everything should be 1
+        // similarly for 2 ...
+        // so here what we will do when mid = 0 so we will swap and mid and low increment...
+        // why because mid should point to 1 and low - 1 should be 0 that is why
         while (mid <= high) {
             if (arr[mid] == 0) {
                 swap(arr, low, mid);
                 low++;
                 mid++;
             } else if (arr[mid] == 1){
+                // mid should point to 1 according to algo.....
                 mid++;
             } else {
+                // 
                 swap(arr, mid, high);
                 high--;
             }
